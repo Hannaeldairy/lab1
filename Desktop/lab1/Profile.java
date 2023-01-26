91537ece631d8cd097c5cf7a7db94c7f410ddfba
hanna <dairyhanna10@gmail.com>
2023-01-26 21:01:58 +0200

    My hobby is added

--- Desktop/lab1/Profile.java
+++ Desktop/lab1/Profile.java
@@ -1,8 +1,9 @@
 public class Problem{
 	public static void main(String[]args){
 		System.out.print("Hanna El Dairy");
-		system.out.println("Mechatronics Engineering")
+		System.out.println("Mechatronics Engineering")
 		System.out.print("First year");
-		//this is a comment;
+		System.out.print("football");
+		
 	}
 }

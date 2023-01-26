30cf746fb80ffd0e7a4de42598988816b9fc62d4
hanna <dairyhanna10@gmail.com>
2023-01-26 20:58:19 +0200

    my profile

---
+++ Desktop/lab1/Profile.java
@@ -0,0 +1,8 @@
+public class Problem{
+	public static void main(String[]args){
+		System.out.print("Hanna El Dairy");
+		system.out.println("Mechatronics Engineering")
+		System.out.print("First year");
+		//this is a comment;
+	}
+}
